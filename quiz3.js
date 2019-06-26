56e96881a874daaee22bafecf2c8113b83d60931
Amritpal Singh <748914@student.tor.loc>
2019-06-26 17:14:15 -0400

    second commit

---
+++ quiz3.js
@@ -0,0 +1,8 @@
+function waitingFn(TimeInMs){
+    const functionTime =Date.now() + timeInMs;
+   
+    while(functionTime=Date.now()+timeInMs;
+}
+}
+ waitingFn(3000);
+console.log("Function call has just ended");
